5f19784def183945be48d0c23e082878bae51565
David Mendoza <dmendoza99@unitec.ede>
2019-07-11 17:10:07 -0600

    branch

---
+++ src/Views/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+class Profile extends Component {
+  render() {
+    return (
+      <View>
+        <Text>wenas</Text>
+      </View>
+    );
+  }
+}
+
+export default Profile;
